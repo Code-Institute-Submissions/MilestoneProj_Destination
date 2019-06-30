28b1501df03a88bdf03cc90bdb8e01845fb2eae3
Ubuntu <ubuntu@ip-172-31-94-151.ec2.internal>
2019-06-30 09:58:31 +0000

    first commit

---
+++ js/maps.js
@@ -0,0 +1,10 @@
+ function initMap(){
+     var options={
+         zoom: 5,
+         center: {lat:46.2276,lng:-2.2137}
+     }
+            var map = new google.maps.Map(document.getElementById("map"), options);
+            
+          //  var Autocomplete = new google.maps.Autocomplete(document.getElementById("Autocomplete"))     
+ }      
+         
