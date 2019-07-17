830875c7062882426a33c4b5dd230d41c9638651
Ubuntu <ubuntu@ip-172-31-94-151.ec2.internal>
2019-07-17 21:31:49 +0000

    markers

--- js/maps.js
+++ js/maps.js
@@ -1,15 +1,22 @@
  var infowindow;
- var service;
- //var map;
+var service;
+
  var marker;
  var input;
  var place;
+ var options;
+var map;
+
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ var labelIndex = 0;
+
+ 
  
  function initMap(){
      var options= {lat:53.3498,lng:-6.2603};
     
-          var map = new google.maps.Map(document.getElementById("map"),{
-             zoom: 8,
+           map = new google.maps.Map(document.getElementById("map"),{
+             zoom: 13,
              center: options,
     //    var maker = new google.maps.Marker({
       //       position:  {lat:53.3498,lng:-6.2603},
@@ -25,14 +32,15 @@
     autocomplete.bindTo('bounds', map);
     
     
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+   // var labels = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
     
-    var infowindow = new google.maps.InfoWindow();
+  //  var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker({
      // label: labels[i % labels.length],
         position: options,
         map: map,
-        anchorPoint: new google.maps.Point(0, -29)
+        title:"place"
+       // anchorPoint: new google.maps.Point(0, -29)
     });
            
         //    google.maps.event.addListener(autocomplete, 'place_changed', function(){
@@ -51,64 +59,49 @@
       if (place) {
            // map.fitBounds(placeselected.geometry.viewport);
             map.setCenter(place.geometry.location);
-            map.setZoom(8);
+            map.setZoom(13);
+            marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+            var request = {
+     location: place.geometry.location,
+     radius: 8000,
+     types:['cafe']
+ };
+       var service = new google.maps.places.PlacesService(map);
+ 
+       service.nearbySearch(request,callback);
         } else {
             map.setCenter(place.geometry.location);
-            map.setZoom(8);
+            map.setZoom(13);
         }
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-})
-    
-    
-    
-    function searchSelected(){
-        var request ={
-        
-        location: place,
-        radius : 500,
-        types:['lodging']
-        
-    };
-        var service = new google.maps.places.PlacesService(map);
-    	service.nearbySearch(request, callback);
-        
-    }
-     function callback(results, status) {
-      if (status != google.maps.places.PlacesServiceStatus.OK) {
-        return;
-      } else {
-        createMarkers(results);
-      }
-    }
+   
 
-    function createMarkers(places) {
-      var bounds = new google.maps.LatLngBounds();
-
-      for (var i = 0, place; place = places[i]; i++) {
-        var image = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        var marker = new google.maps.Marker({
-          map: map,
-          icon: image,
-          title: place.name,
-          position: place.geometry.location
-        });
-
-        bounds.extend(place.geometry.location);
-      }
-      map.fitBounds(bounds);
-    }
-
-    window.google.maps.event.addDomListener(window, 'load', initMap);
+}) ;
+ 
 
 
+ }
+//var labels = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+ 
+ function callback(results, status){
 
-}      
-         
+     if(status == google.maps.places.PlacesServiceStatus.OK){
+      
+         for(var i= 0; i<results.length; i++){
+          
+         //   label: labels[i % labels.length]
+             createrMarker(results[i]);
+         }
+     }
+ }
+// var labels = String.fromCharCode("A".charCodeAt(0) + index)
+ function createrMarker(place){
+     var placePos = place.geometry.location;
+     var marker = new google.maps.Marker({
+         label: labels[labelIndex++ % labels.length],
+         map: map,
+         position: place.geometry.location,
+         title:place.name
+     })
+ }
+ //google.maps.event.addDomListener(window, 'load', initMap);
