b2894cebb9318f1f8216550cd70505d8a9da67a0
Luana Andrade <luzi015@hotmail.com>
2020-01-26 22:40:45 +0000

    markers

--- js/maps.js
+++ js/maps.js
@@ -29,6 +29,7 @@ var labelIndex = 0;
 
 
 function initMap() {
+
     var options = {lat: 53.3498, lng: -6.2603};
 
     map = new google.maps.Map(document.getElementById("map"), {
@@ -36,8 +37,8 @@ function initMap() {
         center: options,
     });
 
-   // input = document.getElementById('autocomplete');
-   // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // input = document.getElementById('autocomplete');
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
     //autocomplete.bindTo('bounds', map);
@@ -58,54 +59,72 @@ function renderMap(){
         }
     });
 
+    var geo   = new google.maps.Geocoder();
+    var LocLat   = 0;
+    var LocLng   = 0;
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: city,
-        zoom: 13
-    });
+    geo.geocode({'address': city}, function(results, status) {
+        if (status === 'OK')
+        {
+
+            LocLat   = results[0].geometry.location.lat();
+            LocLng   = results[0].geometry.location.lng();
+
+
+            var cities = new google.maps.LatLng(LocLat, LocLng);
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: cities,
+                zoom: 13
+            });
 
             //console.log(selectedTypes);
 
-    var request = {
-        location: city.geometry.location,
-        radius: 8000,
-        types: selectedTypes
-    };
+            var request = {
+                location: cities,
+                radius: 8000,
+                types: selectedTypes
+            };
 
-    infowindow = new google.maps.InfoWindow();
+            infowindow = new google.maps.InfoWindow();
 
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, callback);
+        }
+        else
+        {
+            alert('Please choose a type of entertainment');
+        }
+    });
 }
 
-    function callback(results, status) {
+function callback(results, status) {
 
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-            for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < results.length; i++) {
 
-                //   label: labels[i % labels.length]
-                createrMarker(results[i]);
-            }
+            //   label: labels[i % labels.length]
+            createrMarker(results[i]);
         }
     }
+}
 
-    function createrMarker(city,icon) {
-        var placeLoc = city.geometry.location;
+function createrMarker(cities,icon) {
+    var placeLoc = cities.geometry.location;
 
-        var marker = new google.maps.Marker({
-            map: map,
-            position: city.geometry.location,
-            icon: {
-                url: icon,
-                scaledSize: new google.maps.Size(20, 20) // pixels
-            },
-            animation: google.maps.Animation.DROP
-        });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: cities.geometry.location,
+        icon: {
+            url: icon,
+            scaledSize: new google.maps.Size(100, 20)
+        },
+        animation: google.maps.Animation.DROP
+    });
 
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(city.name+ '<br>' +city.vicinity);
-            infowindow.open(map, this);
-        });
-    }
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(cities.name+ '<br>' +cities.vicinity);
+        infowindow.open(map, this);
+    });
+}
